52fbc2fe46afc76295280ee6652870a4818e7c5a
松岡稔明 <>
2025-05-15 14:56:28 +0900

    Sample002を追加

---
+++ Assets/Sample002/Sample002_0.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sample002_0 : MonoBehaviour
+{
+    public void ButtonCallback()
+    {
+        Debug.LogErrorFormat("on");
+    }
+}
---
+++ Assets/Sample002/Sample002_1.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Sample002_1 : MonoBehaviour
+{
+    public void Button(int id)
+    {
+        Debug.LogErrorFormat($"on : {id}");
+    }
+
+
+}
