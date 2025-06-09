fe394bcfd0f53ca3cd5918a20500276e990cc56e
松岡稔明 <>
2025-06-09 16:02:42 +0900

    Sample012追加

---
+++ Assets/Sample012/Sample012_Test1.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sample012_Test1 : SceneBase
+{
+    [SerializeField]
+    Button[] buttons;
+
+    void Start()
+    {
+        for(var i = 0; i < buttons.Length; i++)
+        {
+            var id = i;
+            buttons[i].onClick.AddListener(() => OnButtonCallBack(id));
+        }
+    }
+
+    private void OnButtonCallBack(int id)
+    {
+        switch(id)
+        {
+            case 0:
+            {
+                SceneChange(SceneID.Test2);
+            }
+            break;
+            case 1:
+            {
+                SceneChange(SceneID.Test3);
+            }
+            break;
+            case 2:
+            {
+                SceneChange(beforeScene);
+            }
+            break;
+        }
+    }
+}
